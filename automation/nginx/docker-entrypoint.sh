7dd10d5fe514aabe5af5510dd621764df230bfd9
jeff <jeff@mux4.com>
2016-12-30 15:45:52 -0500

    Add docker-compose.yml

---
+++ automation/nginx/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+envsubst "\
+  \$ALLOWED_HOSTS \
+  \$COUCHPOTATO_HOST \
+  \$COUCHPOTATO_PORT \
+  \$DELUGE_HOST \
+  \$DELUGE_PORT \
+  \$GHOST_PORT \
+  \$HEADPHONES_HOST \
+  \$HEADPHONES_PORT \
+  \$HTTPS_PORT\
+  \$HTTP_PORT \
+  \$SICKBEARD_HOST \
+  \$SICKBEARD_PORT \
+  \$SSL_CERTIFICATE \
+  \$SSL_CERTIFICATE_KEY \
+  \$SYNOLOGY_HOST \
+  \$SYNOLOGY_INTERNAL_HOST \
+  \$SYNOLOGY_PORT \
+  \$WEB_INTERNAL_HOST \
+  " \
+  < /etc/nginx/nginx.conf.tmpl > /etc/nginx/nginx.conf
+
+exec nginx 
