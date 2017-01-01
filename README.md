1ade4c22d55e260a964591ecd7e4451a80c40223
jeff <jeff@mux4.com>
2017-01-01 04:47:57 -0500

    Change from self-signed to letsencrypt ssl certs, and misc changes

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ### Set up notes
 * Decrypt the repository's encrypted content using [transcrypt](https://github.com/elasticdog/transcrypt).
-* Instructions on generating self-signed SSL cert [here](https://rogerhub.com/~r/code.rogerhub/infrastructure/474/signing-your-own-wildcard-sslhttps-certificates)
+* SSL certs generated with [cerbot](https://certbot.eff.org/)
--- automation/nginx/docker-entrypoint.sh
+++ automation/nginx/docker-entrypoint.sh
@@ -2,22 +2,22 @@
 
 envsubst "\
   \$ALLOWED_HOSTS \
-  \$COUCHPOTATO_HOST \
-  \$COUCHPOTATO_PORT \
-  \$DELUGE_HOST \
-  \$DELUGE_PORT \
   \$GHOST_PORT \
-  \$HEADPHONES_HOST \
-  \$HEADPHONES_PORT \
   \$HTTPS_PORT\
   \$HTTP_PORT \
-  \$SONARR_HOST \
-  \$SONARR_PORT \
+  \$MOVIE_HOST \
+  \$MOVIE_PORT \
+  \$MUSIC_HOST \
+  \$MUSIC_PORT \
   \$SSL_CERTIFICATE \
   \$SSL_CERTIFICATE_KEY \
   \$SYNOLOGY_HOST \
   \$SYNOLOGY_INTERNAL_HOST \
   \$SYNOLOGY_PORT \
+  \$TORRENT_HOST \
+  \$TORRENT_PORT \
+  \$TV_HOST \
+  \$TV_PORT \
   \$WEB_INTERNAL_HOST \
   " \
   < /etc/nginx/nginx.conf.tmpl > /etc/nginx/nginx.conf
